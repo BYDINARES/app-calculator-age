6d5ec9cfd9235ef118c130cf9c38b251d57fc6e1
David Gutierrez <dagd0918@gmail.com>
2024-12-18 08:42:36 -0300

    Testing of the JavaScript code, not so much changes

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function dateToYearsMonthsDays() {
     //Input dates//
     
     const dayImput = document.querySelector('.day-input');
-    const inputDay = parseInt(dayImput.value, 10);
+    const inputDay = Number(dayImput?.value);
 
     const monthImput = document.querySelector('.month-input');
-    const inputMonth = parseInt(monthImput.value, 10);
+    const inputMonth = Number(monthImput?.value);
 
     const yearImput = document.querySelector('.year-input');
-    const inputYear = parseInt(yearImput.value, 10);
+    const inputYear = Number(yearImput?.value);
 
 
     //Real dates//
@@ -20,13 +20,26 @@ function dateToYearsMonthsDays() {
     const todaysYear = newDate.getFullYear();
 
     //Erros//
-    const errorAlerts = document.querySelector('.errors');
+    const dayError = document.querySelector('.day-error');
+    const monthError = document.querySelector('.month-error');
+    const yearError = document.querySelector('.year-error');
 
-    
-    if (inputYear > todaysYear) {
+    //Validating
+    if (inputYear > todaysYear || inputYear < 1) {
+        errorAlerts.textContent = 'Invalid year. Please enter a past or the actual year';  
         errorAlerts.style.visibility = 'visible';
-        console.log('An error');
+        return;
+    } else if (inputMonth < 1 || inputMonth > 12) {
+        errorAlerts.textContent = 'Invalid month. Please enter a month between 1-12';
+        errorAlerts.style.visibility = 'visible';
+        return;
+    } else if (inputDay < 1 || inputDay > 31){
+        errorAlerts.textContent = 'Invalid day. Please enter a day between 1-31';
+        errorAlerts.style.visibility = 'visible';
+        return;    
     } else {
         errorAlerts.style.visibility = 'hidden';
     }
+
+    console.log('hello');
 }
